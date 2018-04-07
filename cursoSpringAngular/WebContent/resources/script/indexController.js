44f23c584ce0773d9a79515c64b7f10c791b986a
ivana-queiroz-assis <ivanaqueirozassis@hotmail.com>
2018-04-07 18:46:28 -0300

    Aula 35

--- cursoSpringAngular/WebContent/resources/script/indexController.js
+++ cursoSpringAngular/WebContent/resources/script/indexController.js
@@ -1,5 +1,20 @@
 var app= angular.module('loja',[]);
 
-app.controller('indexController', ['$scope', function($scope){
-	$scope.user= {nome: 'João'};
+app.config(function($routeProvider){
+	$routeProvider
+	.when("/", {controller: "listController", templateUrl:"list.html"})
+	.when("edit/name",{controller: "editController", templateUrl:"form.html"})
+	.when("/new", {controller:"newController",templateUrl:"form.html"})
+	.otherwise({redirectTo:"/"});
+	
+});
+
+app.run(function($rootScope){
+	$rootScope.frutas = ['banana', 'melancia', 'pera']
+});
+
+app.controller('listaController', ['$scope', function($scope){
+	
 }]);
+
+function listController($scope){}
